76f567dc3d033a858f9f2d9a7cf25f0eed32a9b6
AnthonySinitsa <itonyemail@gmail.com>
2023-05-23 00:09:38 +0000

    database running

--- server.js
+++ server.js
@@ -3,11 +3,47 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+mongoose.connect(process.env.DB_URL);
+
+const Books = require('./models/book');
+
+// add validation to confirm we are wired up to our mongo DB
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
+
+const PORT = process.env.PORT || 3002;
+
+app.get('/', (request, response) => {
+  response.status(200).send('Welcome!');
+});
+
+app.get('/books', getBook);
+
+async function getBook(req, res, next) {
+  try {
+    let results = await Books.find();
+    console.log('rESULTS: ', results);
+    res.status(200).send(results);
+  } catch (err) {
+    next(err)
+  }
+}
+
+app.get('*', (request, response) => {
+  response.status(404).send('Not availabe');
+});
+
+app.use((error, request, response, next) => {
+  res.status(500).send(error.message);
+});
 
 app.get('/test', (request, response) => {
 
