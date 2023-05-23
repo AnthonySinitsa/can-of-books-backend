9fcea27ea216804dbdc265348e0d881f812eb31b
AnthonySinitsa <itonyemail@gmail.com>
2023-05-23 22:04:00 +0000

    dleted and post

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.use (express.json());
+app.use(express.json());
 
 const mongoose = require('mongoose');
 // console.nodemonlog(process.env.DB_URL);
@@ -28,12 +28,34 @@ app.get('/', (request, response) => {
 });
 
 app.get('/books', getBook);
+app.post('/books', postBook);
+app.delete('/books/:id', deleteBook);
 
 async function getBook(req, res, next) {
   try {
     let results = await Books.find();
     console.log('rESULTS: ', results);
     res.status(200).send(results);
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function postBook(req, res, next) {
+  console.log(req.body)
+  try {
+    let createdBook = await Books.create(req.body);
+    res.status(200).send(createdBook);
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function deleteBook(req, res, next) {
+  try {
+    let id = req.params.id;
+    await Books.findByIdAndDelete(id);
+    res.send('Book Deleted');
   } catch (err) {
     next(err);
   }
@@ -47,10 +69,8 @@ app.use((error, request, response, next) => {
   response.status(500).send(error.message);
 });
 
-app.get('/test', (request, response) => {
-
-  response.send('test request received');
-
-});
+// app.get('/test', (request, response) => {
+//   response.send('test request received');
+// });
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
